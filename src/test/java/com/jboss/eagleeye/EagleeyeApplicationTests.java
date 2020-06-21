da0566c51487656c22247d7698ae73d602f0ba87
JBoss <abolade.akintomiwa@gmail.com>
2020-06-21 19:35:38 +0100

    Eagle eye setup

---
+++ src/test/java/com/jboss/eagleeye/EagleeyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jboss.eagleeye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EagleeyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
